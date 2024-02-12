38af42e2b69e815805659ccd571df63dc931ef1a
prahul25 <palrahulkumar2002@gmail.com>
2024-02-12 15:05:38 +0530

    gitignore

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+require('dotenv').config();
 const express = require("express")
 const app = express()
 const {getUserData ,getUserDataByUiid, getUserDataByFilter} = require("./controller/user.controller")
 
-const PORT = 3000;
+const Port = 3000;
 // console.log("h")
 app.get("/users" , getUserData)
 app.get("/users/search", getUserDataByFilter)
 app.get("/users/:uuid" , getUserDataByUiid)
+// console.log(process.env)
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${Port}`)
 })
+
